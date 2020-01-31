badf2597f6176c62e1eb83a4d7dcf3368b46379f
Hadia Iftikhar <55398310+HadiaIftikhar-HIi@users.noreply.github.com>
2020-01-31 18:29:08 +0500

    Add files via upload

---
+++ new member detail(task1.4).vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim name As String
+        Dim memberID As Integer
+        Dim more As Char
+        more = "N"
+        name = ""
+        memberID = 0
+        FileOpen(1, "memberinfo.txt", OpenMode.Append)
+        Do
+            Console.Write("Enter name:")
+            name = Console.ReadLine
+            Console.Write("Enter memberID:")
+            memberID = Console.ReadLine
+            WriteLine(1, name)
+            WriteLine(1, memberID)
+
+            Console.Write("Do you want to enter another record?(y/n):")
+            more = Console.ReadLine
+        Loop Until UCase(more) = "N"
+        FileClose(1)
+    End Sub
+
+End Module

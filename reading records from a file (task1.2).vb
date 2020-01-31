d739bfbde9ae82e0b4db3159be615ddc86494e2d
Hadia Iftikhar <55398310+HadiaIftikhar-HIi@users.noreply.github.com>
2020-01-31 18:17:48 +0500

    Add files via upload

---
+++ reading records from a file (task1.2).vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim name As String
+        Dim memberID As Integer
+        name = ""
+        memberID = 0
+        FileOpen(1, "studentinfo.txt", OpenMode.Input)
+        While Not EOF(1)
+            Input(1, name)
+            Input(1, memberID)
+            Console.WriteLine("student name:" & name)
+            Console.WriteLine("student memberID:" & memberID)
+        End While
+        FileClose(1)
+        Console.ReadKey()
+    End Sub
+
+End Module

ec38d229c0d16b1725190c477abe9a84ea573af5
Hadia Iftikhar <55398310+HadiaIftikhar-HIi@users.noreply.github.com>
2020-01-31 18:28:34 +0500

    Add files via upload

---
+++ searching a record (task1.3).vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim name As String
+        Dim memberID As Integer
+        Dim isfound As Boolean
+        Dim searchname As String
+        searchname = ""
+        isfound = False
+        name = ""
+        memberID = 0
+        Console.Write("enter name to search:")
+        searchname = Console.ReadLine
+        searchname = UCase(searchname)
+        FileOpen(1, "memberinfo.txt", OpenMode.Input)
+        While Not EOF(1)
+            Input(1, name)
+            Input(1, memberID)
+            If UCase(name) = searchname Then
+                isfound = True
+                Console.WriteLine("member name:" & name)
+                Console.WriteLine("memberID:" & memberID)
+            End If
+        End While
+        FileClose(1)
+        If isfound = False Then
+            Console.WriteLine("member name and ID can not be found")
+        End If
+        Console.ReadKey()
+    End Sub
+
+End Module
